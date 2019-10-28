5fbec355fbd43245a47cb5c2ebb1ecd180d4636c
mfernandag <mfernandagoracy@gmail.com>
2019-10-28 03:17:23 -0300

    bastante avanzado

--- public/main.js
+++ public/main.js
@@ -3,8 +3,30 @@ const endpoint = `https://www.mocky.io/v2/5cfbb853300000da1d0a8bd2`
 fetch(endpoint)
 .then(response => response.json())
 .then( res => {
-    const {data} = res
-    console.log(data)
+    const imgContainer = document.getElementById('newsContainer')
+    
+    res.forEach(e => {
+        const newsLink = document.createElement('a')
+        newsLink.href = e.url
+        newsLink.classList.add('imgAnchor')
+        const img = document.createElement('img')
+        img.src = e.image
+        img.classList.add('newsImg')
+        const title = document.createElement('a')
+        title.innerText = e.title
+        title.href = e.title
+        title.classList.add('newsTitle')
+        newsLink.appendChild(img)
+        newsLink.appendChild(title)
+        imgContainer.appendChild(newsLink)
+    })
+    /*res.forEach(e => {
+        const title = document.createElement('a')
+        title.innerText = e.title
+        title.href = e.title
+        title.classList.add('newsTitle')
+        imgContainer.appendChild(title)
+    })*/
 })
 
 
