c1661cfe798a14e09871a88e51f562d54ccc6336
mfernandag <mfernandagoracy@gmail.com>
2019-10-27 23:50:50 -0300

    node y dependencias

--- public/main.js
+++ public/main.js
@@ -2,4 +2,10 @@ const endpoint = `https://www.mocky.io/v2/5cfbb853300000da1d0a8bd2`
 
 fetch(endpoint)
 .then(response => response.json())
-.then( res => res.data.forEach(e => console.log(e)))
+.then( res => {
+    const {data} = res
+    console.log(data)
+})
+
+
+
